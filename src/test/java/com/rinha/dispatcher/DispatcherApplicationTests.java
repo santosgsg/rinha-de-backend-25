0aae9c9f6612ff57c4e7ffd6a3e4b8983b7793c1
Gabriel <gabrielsantos.s.guedes@gmail.com>
2025-07-22 23:24:13 +0100

    add spring base structure

---
+++ src/test/java/com/rinha/dispatcher/DispatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rinha.dispatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
